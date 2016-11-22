1b3c3509c0a875bb96e0c804bde2047faa70a1b2
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2016-11-22 21:34:23 +0200

    overrde label

--- TickTack/src/main/scala/codespace/ticktack/strategies/BasePlayer.scala
+++ TickTack/src/main/scala/codespace/ticktack/strategies/BasePlayer.scala
@@ -2,5 +2,5 @@ package codespace.ticktack.strategies
 
 import codespace.ticktack.{Label, Player}
 
-abstract class BasePlayer(l:Label) extends Player
+abstract class BasePlayer(val label:Label) extends Player
 
