bb0daddab38f48dd8f17772983f8b7855cf92352
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2016-12-08 21:34:44 +0200

    added cmdmain

---
+++ TickTack/src/main/scala/codespace/ticktack/CmdMain.scala
@@ -0,0 +1,15 @@
+package codespace.ticktack
+
+object CmdMain extends Game {
+
+  val rules = ThreeRules
+
+  def main(args:Array[String]):Unit =
+  {
+    val aPlayer = new strategies.Recursive(ToeLabel,rules)
+    val bPlayer = new strategies.Recursive(CrossLabel,rules)
+    val field = play(aPlayer,bPlayer)
+    println(field)
+  }
+
+}
