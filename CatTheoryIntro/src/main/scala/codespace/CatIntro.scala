d2efa317aa47b63588efe5ee2819ccfd9421bd74
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2016-12-08 19:24:03 +0200

    added notes about cat-intro

--- CatTheoryIntro/src/main/scala/codespace/CatIntro.scala
+++ CatTheoryIntro/src/main/scala/codespace/CatIntro.scala
@@ -1,5 +1,7 @@
 package codespace
 
+import scala.annotation.tailrec
+
 object CatIntro {
 
 
@@ -12,16 +14,22 @@ object CatIntro {
 
     def map[A,B](a:F[A])(f:A=>B):F[B]
 
-    /*
-    def compose[G[_]](g:Functor[G]):Functor[F[G]] =
+    // map(f:A=>B)(a:F[A]):F[B]
+    // map(f:A=>B): F[A]=>F[B]
+
+
+
+/*
+    def compose[G[_]](g:Functor[G]):Functor[({type L[X]=F[G[X]]})#L] =
       new Functor[F[G]] {
         def map[A,B](a:F[G[A]])(f:A=>B):F[G[B]] =
         {
-          //val f:G[A]=>G[B]
+
+
           self.map[G[A],G[B]](a)((x:G[A])=>g.map(x)(f)) // :  (G[A]=>G[B])=>F[G[B]]
         }
       }
-      */
+*/
 
   }
 
@@ -30,6 +38,7 @@ object CatIntro {
   {
     def map[A,B](a:Option[A])(f:A=>B):Option[B] = a map f
   }
+
 
   object ListFunctor extends Functor[List]
   {
@@ -58,6 +67,8 @@ object CatIntro {
 
   }
 
+
+
   trait Applicative[F[_]] extends Apply[F]
   {
     def pure[A](a:A):F[A]
@@ -65,18 +76,69 @@ object CatIntro {
     def pureFun[A,B](f:A=>B):F[A=>B]=pure(f)
   }
 
+  abstract class A
+  {
+    type B
+  }
+
+
+  //val x: A#B = 1
+  //var y: A#B = 2
+
+
+
+  //List[({ type L[X]=Map[Int,X]  })#L[Int]]
+
+  //Applicative[Map[Int,_]]
+
   trait Monadic[F[_]] extends Functor[F]
   {
 
-    def map[A,B](f:A=>B):F[A] => F[B]
+    def map[A,B](f:A=>B):(F[A] => F[B])
 
-    def flatMap[A,B](f:A=>F[B]): F[A] => F[B]
+
+
+    def flatten[A](ffa:F[F[A]]):F[A]
+
+    def flatMap[A,B](f:A=>F[B]): (F[A] => F[B])  = {
+      (fa:F[A]) =>
+        flatten(map(f)(fa))
+    }
 
   }
+
+  //
+  def f():Int = {
+    var x = 1
+    for (i <- 1 to 10) {
+      x = x * 2
+    }
+    x
+  }
+
+  //Some[X] <: Option[X]
+  //val x: Option[Int] = new Some(1)
+
+  def f1():Int = {
+    @tailrec
+    def s(i:Int, x:Int): Int =
+      if (i>10) x else s(i+1,x*2)
+    s(1,1)
+  }
+
+  //Int => Int
+
+     //F:(A,B)=>C
+     // curryF(a): b => f(a,b)
+
+     //g:(A=>(B=>C))
+
 
 
   //def g[F]:(f:Monadic[F]) {
   //}
+
+
 
 
 }
--- CatTheoryIntro/src/main/scala/codespace/Implicits.sc
+++ CatTheoryIntro/src/main/scala/codespace/Implicits.sc
@@ -1,25 +1,83 @@
 import scala.languageFeature.implicitConversions
 
+//x.sqrt
 
-def f(a:String):Int = {
-  Integer.parseInt(a)
-}
+// E x: p(x)
+// A x: ~p(x) = ~ E x: p(x)
+// forall(p) = not exists (not p)
+// exists p = not forall (not p)
 
 
-def g(b:Int)(implicit transformer:String=>Int):Unit =
+case class Point(x:Int, y:Int)
+
+
+object Point
 {
 
-  val x:Int = "123"
-  System.out.println(s"b % 2 == ${b%2}")
+
+  trait PointAsNumeric extends Numeric[Point] {
+    override def plus(x: Point, y: Point): Point =
+      Point(x.x + y.x,x.y+y.y)
+
+    override def minus(x: Point, y: Point): Point = ???
+
+    override def times(x: Point, y: Point): Point = ???
+
+    override def negate(x: Point): Point = ???
+
+    override def fromInt(x: Int): Point = ???
+
+    override def toInt(x: Point): Int = ???
+
+    override def toLong(x: Point): Long = ???
+
+    override def toFloat(x: Point): Float = ???
+
+    override def toDouble(x: Point): Double = ???
+
+    override def compare(x: Point, y: Point): Int = ???
+  }
+
 
 }
 
-val x: Either[Throwable,Int] = ???
-var y: Either[String,Int]
 
-y = x.left.map(_.getMessage)
+object Sqrt
+{
 
-val getMessage: Throwable => String = _.getMessage
+  def apply[T](x:T)(implicit n:Numeric[T]):Double =
+  {
+    def v(s:Double,xn:Double) =
+      (xn+s/xn)/2
 
-//val x = "123"
-g(123)(f)
+    var done = false
+    var xp = 1.0
+    val s = n.toDouble(x)
+    while(!done) {
+      val xc = v(s,xp)
+      done = Math.abs(xc-xp) < 0.001
+      xp = xc
+    }
+    xp
+  }
+
+
+}
+
+Sqrt(200)
+
+implicit class WithSqrt[X](x:X)(implicit n:Numeric[X])
+{
+
+  def sqrt():Double = Sqrt(x)
+
+  def ** (y:X):X = n.minus(x,y)
+
+}
+
+
+val x = 100
+
+x ** 99
+
+x.sqrt()
