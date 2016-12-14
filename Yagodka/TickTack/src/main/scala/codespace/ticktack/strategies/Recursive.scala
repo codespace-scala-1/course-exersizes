33f617737793ce287cb393a2df90708e95d4ef53
Yagodka Andrey <yagodka.andrey@gmail.com>
2016-12-15 01:35:22 +0200

    fix 2 error

--- Yagodka/TickTack/src/main/scala/codespace/ticktack/strategies/Recursive.scala
+++ Yagodka/TickTack/src/main/scala/codespace/ticktack/strategies/Recursive.scala
@@ -7,9 +7,8 @@ class Recursive(label:Label, rules: Rules) extends BasePlayer(label,rules) {
   override def nextStep(f: Field): Either[String,((Int, Int), Player)] =
   {
     f.get(1,1) match {
+      case Some(_) => checkNearest(1,1,f)
       case None => Right((1,1),this)
-      case Some(l) if (l==label)  =>
-           checkNearest(1,1,f)
     }
   }
 
@@ -24,10 +23,10 @@ class Recursive(label:Label, rules: Rules) extends BasePlayer(label,rules) {
            if (retval.isDefined) {
              return Right((retval.get,this))
            }
-         case Some(l1) =>
+         case Some(_) =>
        }
      }
-     Right("Can't ")
+     Left("Can't ")
   }
 
   def checkNextStep(x:Int,y:Int,f:Field):Option[(Int,Int)]=
@@ -40,7 +39,7 @@ class Recursive(label:Label, rules: Rules) extends BasePlayer(label,rules) {
 
   override def tell(s: String): Player =
   {
-    Console.println(s"Player ${label} receive message $s")
+    Console.println(s"Player $label receive message $s")
     this
   }
 
