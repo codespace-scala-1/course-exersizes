45f17534818ba6ac095fac519922cc2df4f69f7b
vukelan <vukelan@gmail.com>
2016-12-21 01:35:57 +0200

    implemented words to digits hashmap

--- vukelan/PhoneCode/data/test.w
+++ vukelan/PhoneCode/data/test.w
@@ -21,3 +21,4 @@ so
 Tor
 Torf
 Wasser
+ort
--- vukelan/PhoneCode/src/main/scala/codespace/phonecode/App.scala
+++ vukelan/PhoneCode/src/main/scala/codespace/phonecode/App.scala
@@ -4,29 +4,74 @@ import scala.io.Source
 
 object App {
   def main(args: Array[String]): Unit = {
-
+    // read file with codes
     val phonecode = Source.fromFile("/Users/johnsmith/IdeaProjects/course-exersizes/vukelan/PhoneCode/data/phonecode.txt", "UTF-8")
-    val lineIterator = phonecode.getLines
+    val linesPhonecode = phonecode.getLines
     val numPattern = "[0-9]+".r
-    val letterPattern = "[a-z,A-Z]+".r
+    val letPattern = "[a-z,A-Z]+".r
 
+    // map characters to digits
+    val charToDigit = linesPhonecode
+      .map(
+        x => (
+          numPattern.findFirstIn(x).getOrElse(""),
+          letPattern.findAllIn(x).mkString
+        )
+      )
+      .flatMap(
+        x => for (c <- x._2; n = x._1) yield c -> n.toInt
+      )
+      .toMap
 
+    // read words and transforming them to numbers
+    val words =
+      Source
+        .fromFile("/Users/johnsmith/IdeaProjects/course-exersizes/vukelan/PhoneCode/data/test.w")
+        .getLines()
 
-    val digitLetters = for (l <- lineIterator) {
+    val wordsTranslated = words
+      .map(word =>
+        (word, for (char <- word.replace(""""""","").toLowerCase()) yield charToDigit.getOrElse(char, "+"))
+      )
+      .map(x => (x._1, x._2.mkString))
+      .filter(!_._2.contains("+"))
+      .map(x => (x._2, x._1))
+      .toMap
+    // output test
+    // println(wordsTranslated.mkString("--\n"))
+
+    // ***---***
+    // read telephone numbers
+    val numbers = Source.fromFile("/Users/johnsmith/IdeaProjects/course-exersizes/vukelan/PhoneCode/data/test.t")
+      .getLines()
+
+    val translatedNumbers = numbers
+      .map(
+        x => (
+          x.replace("-", "").replace("/", ""),
+          wordsTranslated.getOrElse(x.replace("-", "").replace("/", ""), "-")
+        )
+      )
+    println(translatedNumbers.mkString("\n"))
+    println(wordsTranslated.mkString(" "))
+
+    // another map format (word,number)--(next_word..n,next_number..n)
+    val codes = words
+      .flatMap(x => for (i <- x) yield (x, charToDigit.getOrElse(i, "+")))
+    //    println(codes.mkString("--"))
+
+    // unfinished for comprehension varian
+    val digitLetters = for (l <- linesPhonecode) {
       val digit = numPattern.findFirstIn(l)
-      val letters = letterPattern.findAllIn(l)
+      val letters = letPattern.findAllIn(l)
     }
 
-    val m = Map(1 -> ("a", "b"))
-
-    System.out.println(m)
-
-    // find all matches-digits and print them
-    val matches = for (m <- numPattern.findAllIn(lineIterator.mkString)) yield m
+    // print all matches-digits
+    val matches = for (m <- numPattern.findAllIn(linesPhonecode.mkString)) yield m
     System.out.println(matches.mkString("->"))
 
     // print all lines
-    for (line <- lineIterator) {
+    for (line <- linesPhonecode) {
       System.out.println(line)
     }
 
