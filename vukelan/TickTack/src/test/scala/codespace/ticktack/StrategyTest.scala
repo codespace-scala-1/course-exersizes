1e486c52378e33b1c95ac63ee58b25ac92827310
vukelan <vukelan@gmail.com>
2016-12-22 14:48:47 +0200

    strategetest defence debuging

--- vukelan/TickTack/src/test/scala/codespace/ticktack/StrategyTest.scala
+++ vukelan/TickTack/src/test/scala/codespace/ticktack/StrategyTest.scala
@@ -3,14 +3,18 @@ package codespace.ticktack
 import codespace.ticktack.ThreeRules.ThreeField
 import codespace.ticktack.strategies.ComputerPlayer
 import codespace.ticktack.{CrossLabel, ToeLabel}
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-class StrategyTest extends FunSuite {
+class StrategyTest extends FunSuite with BeforeAndAfterAll {
+
+  override def beforeAll(): Unit = {
+    Console.println(player.defence(field))
+  }
 
   // field to test
   val (c, t, n) = (Some(CrossLabel), Some(ToeLabel), None)
   val player = new ComputerPlayer(ToeLabel,ThreeRules)
-  val cells = IndexedSeq(IndexedSeq(c, c, t),
+  val cells = IndexedSeq(IndexedSeq(c, c, n),
                         IndexedSeq(n, n, n),
                         IndexedSeq(t, t, n))
   val field = ThreeField(cells)
@@ -18,6 +22,5 @@ class StrategyTest extends FunSuite {
   test("computer player blocks danger cells") {
     assert(player.nextStep2(field) == Right(0,2))
   }
-
 
 }
