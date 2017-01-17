a67a38d1c2a8dfb423c7677d560bededb175758a
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2017-01-17 20:38:51 +0200

    added overall test

---
+++ shareevent/server/src/test/scala/sharevent/OverallFlowTest.scala
@@ -0,0 +1,29 @@
+package sharevent
+
+
+import shareevent.DomainContext
+//import model._
+
+import org.scalatest._
+
+import scala.util.Try
+
+class OverallFlowTest extends FunSpec {
+
+
+  /*
+  def bestScheduleLocations(scheduleItems: Seq[ScheduleItem]): DomainContext => Try[ScheduleItem] = ???
+
+  def runEvent(organizer: Organizer, title: String): DomainContext => Try[ScheduleItem] =
+    repo => {
+      for {event <- createEvent(organizer, title, theme = "*")(repo)
+           scheduledItems = possibleLocationsForEvent(event)(repo)
+           scheduledItem <- bestScheduleLocations(scheduledItems)(repo)
+      } yield {
+        scheduledItem
+      }
+    }
+    */
+
+
+}
