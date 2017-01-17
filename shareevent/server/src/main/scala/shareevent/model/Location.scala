2eb119b3dee000c32310c588ca0c9769c6239e66
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2017-01-17 21:22:00 +0200

    added booling to location

--- shareevent/server/src/main/scala/shareevent/model/Location.scala
+++ shareevent/server/src/main/scala/shareevent/model/Location.scala
@@ -1,8 +1,23 @@
 package shareevent.model
 
-/**
-  * Created by rssh on 1/17/17.
-  */
-class Location {
+import org.joda.time.Interval
+
+sealed trait BookingStatus
+
+case object BookingStatus {
+
+  case object Preliminary extends BookingStatus
+
+  case object Final extends BookingStatus
 
 }
+
+case class Booking(time: Interval,
+                   event: Event,
+                   status: BookingStatus = BookingStatus.Preliminary)
+
+
+
+case class Location(
+  name:  String,
+  books: Seq[Booking] = Seq())
